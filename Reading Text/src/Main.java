2f4cc37d62d4dd56431387fd4b69731deae88e73
nathanli2003 <nathancheungli@gmail.com>
2020-08-26 17:19:35 -0700

    Reading Text Hand In

---
+++ Reading Text/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> lines = new ArrayList<>();//initialize array
+        ArrayList<Integer> words = new ArrayList<>();
+        FileReader fr = new FileReader("in.txt");//creates file reader object
+        BufferedReader br = new BufferedReader(fr);//creates buffered reader object
+        String line;
+        String input = "Lovelace";   // Input word to be searched
+        while ((line = br.readLine()) != null) {//adds all lines to the lines arraylist
+            lines.add(line);
+        }
+        for (String s : lines) {//goes through each line
+            if(s.contains(input)){//searches line for input
+                words.add(lines.indexOf(s));//adds line with input to the word arraylist
+            }
+        }
+        System.out.println("The input appears in the following index positions " + words);
+        fr.close();
+    }
+}
